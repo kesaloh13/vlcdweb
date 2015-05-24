d1846bcdb3692b26e61bfa4c34be77af69599b9c
unknown <mholasek@nb83mholasek8.bit.local>
2015-05-24 22:15:45 +0200

    Added some additional logging. Added LCd Status, backlight, etc. to socket-data.

--- app.js
+++ app.js
@@ -1,39 +1,21 @@
 /*
-	vlcd.js Prototype, (c) Michael Holasek, 24.5.2015
+	vlcd.js 1.0.1, (c) Michael Holasek, 24.5.2015
 */
 
 var conf = require('./config.json');
-var serialport = require("serialport");				// include serial lib
-var SerialPort  = serialport.SerialPort;			// make a local instance of serial
-var serialData = {};								// object to hold what goes out to the client
+var serialport = require("serialport");				
+var SerialPort  = serialport.SerialPort;
+var serialData = {};								
+var serialDataPrev = {};
+var lcdBacklight = false;
+var lcdStatus = {};
 
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var favicon = require('serve-favicon');
 
-
-/* Start the Webserver */
-server.listen(conf.serverport);
-console.log("Server started on Port:" +conf.serverport)
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/assets/index.html');
-});
-
- /* Provides static files from the /asset directory */
- app.get(/^(.+)$/, function(req, res){ 
-     console.log('static file request : ' + req.params[0]);
-     res.sendFile( __dirname + "/assets/"+ req.params[0]); 
- });
-
-/*favicon doesn't work right now - don't know why ??? */
-app.use(favicon(__dirname + '/assets/favicon.ico'));
-
-
-/* FS for Logging */
-fs=require("fs");
-
+logMessage("Connecting to serialport: " + conf.serialport + " ...")
 
 /* Open serial interface - Change the name of the port in config.json */ 
 // open the serial port. 
@@ -43,28 +25,66 @@ var myPort = new SerialPort(conf.serialport, {
         stopBits: 1,
 	parser: serialport.parsers.readline("\r") 
 });
-  
+
+logMessage("Serial port connected");
+logMessage("Starting webserver on port: " + conf.serverport + " ...");
+
+
+/* Start the Webserver */
+server.listen(conf.serverport);
+logMessage("Webserver started. Waiting for web client ...");
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/assets/index.html');
+});
+
+ /* Provides static files from the /asset directory */
+ app.get(/^(.+)$/, function(req, res){ 
+     logMessage('static file request : ' + req.params[0]);
+     res.sendFile( __dirname + "/assets/"+ req.params[0]); 
+ });
+
+/*favicon doesn't work right now - don't know why ??? */
+app.use(favicon(__dirname + '/assets/favicon.ico'));
+
 
 // listen for new socket.io connections:
 io.sockets.on('connection', function (socket) {
+	var clientIp = socket.request.connection.remoteAddress
+	logMessage("Client connect from: " + clientIp);
 	// if there's a socket client, listen for new serial data:
 	myPort.write(["MC@",String.fromCharCode(13)].join(""));  
 	myPort.on('data', function (data) {
-
 		// set the value property of scores to the serial string:
-		data = [data.slice(8,24),"<br>",data.slice(24,40),"<br>",data.slice(40,56)].join("");
-		serialData.value = data.replace(/\�/g,"&deg;").replace(/\x04/g,">").replace(/\x06/g,"&plusmn;").replace(/\x05/g,"&radic;").replace(/\x01/g,"|").replace(/\x02/g,"|").replace(/\x03/g,"|").replace(/ /g,"&nbsp;");
-	
-		
-		// debugging to Terminal:
-		//console.log(data);
-		// send a serial event to the web client with the data:
-		socket.emit('serialEvent', serialData);
+		if (data.slice(1,2)!="<") {
+			serialData.value= "Waiting for data ...";
+			myPort.write(["MC@",String.fromCharCode(13)].join(""));
+		} else {	
+			data = [data.slice(2,24),"<br>",data.slice(24,40),"<br>",data.slice(40,56)].join("");
+			serialData.value = data.replace(/\�/g,"&deg;").replace(/\x04/g,">").replace(/\x06/g,"&plusmn;").replace(/\x05/g,"&radic;").replace(/\x01/g,"|").replace(/\x02/g,"|").replace(/\x03/g,"|").replace(/ /g,"&nbsp;");
+		}
+
+		// send a serial event to the web client with the data (only if data has changed)
+		if (serialData!=serialDataPrev) { 
+			socket.emit('serialEvent', serialData);
+			serialDataPrev=serialData;
+		}
 	});
 	socket.on('navigation', function (navigation) {
-		//console.log(navigation);
+		//logMessage(navigation);
 		//Send MC#\r to serialport where #=button Code
 		myPort.write(String.fromCharCode(77,67,navigation,13));
 		myPort.write(String.fromCharCode(77,67,0,13));
 	});
+	
+	//Client disconnects
+	socket.on('disconnect', function () {
+		logMessage("Client disconnect from: " + clientIp);
+	})
 });
+
+function logMessage(message) {
+	var datetime = new Date();
+	datetime = datetime.toLocaleString();
+	console.log("[" + datetime + "]: " + message);
+}
